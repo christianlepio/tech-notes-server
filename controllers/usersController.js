db166fb1bdc291f17180d264ecf454fdb1cd7ff2
christianlepio <rylepio79@gmail.com>
2024-01-01 03:23:35 +0800

    create get all users async handler function

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,2 +1,16 @@
+// import data models
 const User = require('../models/User')
 const Note = require('../models/Note')
+// import express-async-handler
+// this is a utility function that simplifies error handling in asynchronous 
+// express js middleware and route handlers
+const asyncHandler = require('express-async-handler')
+// import bcrypt to encrypt password value
+const bcrypt = require('bcrypt')
+
+// @desc Get all users
+// @route GET /users
+// @access Private
+const getAllUsers = asyncHandler(async (req, res) => {
+    
+})
