6728f930410853e0153a41d5282458007669f9e4
christianlepio <rylepio79@gmail.com>
2024-01-03 03:49:55 +0800

    supply http request methods controller function

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const router = express.Router()
+// import notes controller
+const notesController = require('../controllers/notesController')
 
 // this is the index route of '/notes' route
 router.route('/')
-    .get() // fetch notes
-    .post() // create new notes
-    .patch() // update note
-    .delete() // delete note
+    .get(notesController.getAllNotes) // fetch notes
+    .post(notesController.createNewNote) // create new notes
+    .patch(notesController.updateNote) // update note
+    .delete(notesController.deleteNote) // delete note
 
 module.exports = router
