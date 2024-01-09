1636803cf90d22d45ca51731e5c90417208edceb
christianlepio <rylepio79@gmail.com>
2024-01-09 19:05:41 +0800

    implement JWT access token verification to note/user routes

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -2,6 +2,11 @@ const express = require('express')
 const router = express.Router()
 // import notes controller
 const notesController = require('../controllers/notesController')
+// middleware to verify JWT access token
+const verifyJWT = require('../middleware/verifyJWT')
+
+// check if access token exist and not expired before performing controllers below
+router.use(verifyJWT)
 
 // this is the index route of '/notes' route
 router.route('/')
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,11 @@ const express = require('express')
 const router = express.Router()
 // import user controller 
 const usersController = require('../controllers/usersController')
+// middleware to verify JWT access token
+const verifyJWT = require('../middleware/verifyJWT')
+
+// check if access token exist and not expired before performing controllers below
+router.use(verifyJWT)
 
 // this is the index route of '/users' route
 router.route('/')
